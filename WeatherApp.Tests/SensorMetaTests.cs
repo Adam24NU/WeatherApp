4322b7da6b017befa6821c9dad04d6ed6675a6ec
Adam24NU <40627092@live.napier.ac.uk>
2025-04-23 16:54:15 +0100

    Tried to use unit-testing but with mac version it isn’t compatible

---
+++ WeatherApp.Tests/SensorMetaTests.cs
@@ -0,0 +1,23 @@
+﻿using WeatherApp.Models;
+using Xunit;
+
+public class SensorMetaTests
+{
+    [Fact]
+    public void SensorMeta_ShouldInitializeCorrectly()
+    {
+        var sensor = new SensorMeta
+        {
+            SensorID = "001",
+            Location = "55.95,-3.19",
+            Status = "OK",
+            Installed = DateTime.Now,
+            IsFlagged = false
+        };
+
+        Assert.Equal("001", sensor.SensorID);
+        Assert.Equal("55.95,-3.19", sensor.Location);
+        Assert.Equal("OK", sensor.Status);
+        Assert.False(sensor.IsFlagged);
+    }
+}
