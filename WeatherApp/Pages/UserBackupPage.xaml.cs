a293d5f2d3b8a2f4821c798afb7557c9ccf1412c
bartb <40690799@live.ac.uk>
2025-05-05 14:07:38 +0100

    Service Helper created for Permissions to read data / Back up page

---
+++ WeatherApp/Pages/UserBackupPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using WeatherApp.Models;
+
+namespace WeatherApp.Pages
+{
+    public partial class UserBackupPage : ContentPage
+    {
+        public UserBackupPage(List<User> users)
+        {
+            InitializeComponent();
+            UserBackupListView.ItemsSource = users;
+        }
+    }
+}
---
+++ WeatherApp/ServiceHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace WeatherApp
+{
+    public static class ServiceHelper
+    {
+        public static T GetService<T>() =>
+            Current.GetService<T>();
+
+        public static IServiceProvider Current =>
+            MauiApplication.Current.Services;  // ✅ This is the correct way
+    }
+}
